fc868e541b92436d5ec206a24c2430004ceaff48
Carl Ryan Kristopher Calpe <kristopher.calpe12@gmail.com>
2022-06-08 19:04:17 +0800

    Update header.blade.php

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,11 +1,11 @@
 <div class="navbar">
   <div class="navbar-inner">
 
-    <a id="logo" href="/"> Single Malt </a>
+    <a id="logo" href="/"> LOGO </a>
 
     <ul class="nav">
       <li><a href="/"> Home </a>
-      <li><a href="/contact"> Contact </a>
+      <li><a href="/#contact"> Contact </a>
     </ul>
 
   </div>
