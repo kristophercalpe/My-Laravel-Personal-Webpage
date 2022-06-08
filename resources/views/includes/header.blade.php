9a433454c93b2e8b93f7c2e95c9ffdba406eee2f
Carl Ryan Kristopher Calpe <kristopher.calpe12@gmail.com>
2022-06-08 19:08:19 +0800

    Update header.blade.php

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,11 +1,11 @@
 <div class="navbar">
   <div class="navbar-inner">
 
-    <a id="logo" href="/"> LOGO </a>
+    <a id="logo" href="/" style="display: none"> LOGO </a>
 
-    <ul class="nav">
-      <li><a href="/"> Home </a>
-      <li><a href="/#contact"> Contact </a>
+    <ul class="nav" style="display: none">
+      <li><a href="/" style="display: none"> Home </a>
+      <li><a href="/#contact" style="display: none"> Contact </a>
     </ul>
 
   </div>
